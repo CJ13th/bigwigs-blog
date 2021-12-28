0d5a8086cce79c8415ae285170cdab959c67fb48
CJ13th <ccampbell.james@gmail.com>
2021-12-28 00:40:35 +0000

    Style index page and load content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 
         if (res.ok) {
             const {posts} = await res.json()
-            console.log(res.ok)
+
             return {
                 props: {posts}
             }
@@ -23,9 +23,25 @@
 </script>
 
 
-<h1 class="text-4xl mb-10 font-extrabold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1 class="text-4xl mb-10 font-extrabold">Bigwigs Blog</h1>
 
-{#each posts as post}
-    <pre>{post.title}</pre>
+{#each posts as {title, slug, excerpt, coverImage, tags}}
+    <div class="card text-center shadow-2xl mb-20">
+        <figure class="px-10 pt-10">
+            <img src={coverImage.url} alt={`Cover image for ${title}`}>
+        </figure>
+    </div>
+    <div class="card-body">
+        <h1 class="title">{title}</h1>
+        <p>{excerpt}</p>
+        {#each tags as tag}
+            <div class="flex justify-center mt-5 space-x-2">
+                <span class="badge badge-primary">{tag}</span>
+            </div>
+        {/each}
+        <div class="justify-center card-actions">
+            <a href={`/posts/${slug}`} class="btn btn-outline btn-primary">Read more</a>
+        </div>
+    </div>
+
 {/each}
